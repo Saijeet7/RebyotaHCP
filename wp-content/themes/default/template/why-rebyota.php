a061a8b2dac0b3a3ac9d24baeeeb22254d37e800
saijeet7 <saijeet2019@gmail.com>
2022-11-29 12:09:54 +0545

    complete : why rebyota page desktop view

--- wp-content/themes/default/template/why-rebyota.php
+++ wp-content/themes/default/template/why-rebyota.php
@@ -21,9 +21,8 @@ the_post();
         </div>
     </section>
 
-    <div class="backgroundWrapper backgroundWrapper--altba"
+    <div class="backgroundWrapper backgroundWrapper--alt"
         style="background-image: url('<?php echo get_template_directory_uri(); ?>/uploads/backgroundWrapper.jpg')">
-
 
         <section class="sectionTitle">
             <div class="container">
@@ -443,14 +442,17 @@ the_post();
                     <li>Of the 149 participants treated with REBYOTA, 123 (83%) experienced a total of 805
                         treatment-emergent AEs.<sup>4</sup> The majority were related to preexisting conditions or C.
                         <em>difficile</em>
-                        infection </li>
+                        infection
+                    </li>
                 </ul>
                 <div class="fancyCard__textBottom">
                     <p><sup>f</sup>96% (143/149) of patients received 2 REBYOTA doses and 4% (6/149) received 1
                         dose.<sup>4</sup>
-                        <br><sup>g</sup>Dosing in the REBYOTA clinical program, including PUNCH<sup>TM</sup> CD3, was based on data from a
+                        <br><sup>g</sup>Dosing in the REBYOTA clinical program, including PUNCH<sup>TM</sup> CD3, was
+                        based on data from a
                         double-blind, placebo-controlled phase 2b trial in patients with ≥2 rCDI, which showed that a
-                        single REBYOTA dose produced maximal benefit after an rCDI episode.<sup>1,3</sup> The phase 2b study
+                        single REBYOTA dose produced maximal benefit after an rCDI episode.<sup>1,3</sup> The phase 2b
+                        study
                         randomized patients to: 2 REBYOTA doses (group A; n=41), 2 placebo doses (group B; n=44), or 1
                         REBYOTA dose followed by 1 placebo dose (group C; n=42). rCDI prevention for 8 weeks post
                         treatment defined efficacy, with rCDI within 8 weeks rendering patients eligible for up to 2
@@ -461,12 +463,229 @@ the_post();
                     </p>
                 </div>
             </div>
-
+        </section>
     </div>
+
+    <section class="fancyCard">
+        <div class="container">
+            <div class="fancyCard__textDescription">
+                <h2 class="sectionTitle__title">HOW <span class="logo logo--large"><strong>RE</strong>BYOTA</span> WORKS
+                </h2>
+                <p>While the exact mechanism of action is not fully understood…<br>
+                    REBYOTA is thought to involve repopulation and restoration of the composition and diversity
+                    of the gut microbiome to suppress C. difficile outgrowth and CDI recurrence<sup>1</sup></p>
+            </div>
+            <ul class="fancyCard__list">
+                <li>In clinical trials, treatment success was associated with increased relative abundance of
+                    Clostridia- and Bacteroidia-class bacteria and decreased relative abundance of Gammaproteobacteria-
+                    and Bacilli-class bacteria<sup>1</sup>
+                </li>
+            </ul>
+
+            <div class="fancyCard__center">
+                <div class="fancyCard__wrap fancyCard__wrap--alt">
+                    <div class="fancyCard__wrapInner fancyCard__wrapInner--center">
+                        <h3 class="fancyCard__chartHead">
+                            Shift in microbiome diversity following administration
+                            (demonstrated in PUNCH<sup>TM</sup> open-label trial)<sup>4,a</sup>
+                        </h3>
+                        <figure class="fancyCard__chart">
+                            <img src="<?php echo get_template_directory_uri() ?> /uploads/chart.png" alt="Chart" />
+                        </figure>
+                    </div>
+                </div>
+            </div>
+            <div class="fancyCard__textDescription">
+                <p><strong>REBYOTA also showed a shift to secondary bile acid dominance<sup>1</sup></strong></p>
+                <p>REBYOTA administration was also associated with a shift in gut bile acid compositions of secondary
+                    bile acid predominance, which has been associated with suppressions of C. difficile outgrowth and
+                    CDI recurrence in animal studies</p>
+            </div>
+            <div class="fancyCard__textBottom">
+                <p><sup>a</sup>For illustrative purposes only.
+                </p>
+            </div>
+        </div>
     </section>
 
+    <div class="backgroundWrapper"
+        style="background-image: url('<?php echo get_template_directory_uri(); ?>/uploads/backgroundWrapper.jpg')">
+
+        <section class="fancyCard">
+            <div class="container">
+                <h2 class="sectionTitle__title"><span
+                        class="logo logo--large"><strong>RE</strong>BYOTA<sup>®</sup></span><strong> ADMINISTRATION
+                    </strong>
+                </h2>
+                <div class="sectionTitle__title">
+                    <p><span class="logo logo--large"><strong>RE</strong>BYOTA—</span>ONE DOSE.<br>ONE COMPLETE
+                        MICROBIOME
+                        TREATMENT</p>
+                </div>
+                <div class="fancyCard__textDescription">
+                    <p><strong>Delivered within minutes by a healthcare professional.</strong></p>
+                </div>
+
+                <div class="fancyCard__wrap fancyCard__wrap--alt fancyCard__wrap--fullWidth">
+                    <div class="fancyCard__wrapInner fancyCard__wrapInner--padding">
+                        <div class="fancyCard__flex">
+                            <div class="fancyCard__flexContent">
+                                <h3 class="fancyCard__header">
+                                    BEFORE
+                                </h3>
+                                <p>Request that patient empty bladder and bowel, if possible.</p>
+                                <p>Place patient in the preferred position: either a left-side or
+                                    knee-chest position.<sup>1</sup></p>
+                            </div>
+                            <div class="fancyCard__flexContent">
+                                <h3 class="fancyCard__header">
+                                    DURING
+                                </h3>
+                                <p>Lubricate and gently insert the provided administration tube into the rectum about
+                                    5 inches pointed slightly toward the navel.</p>
+                                <p>Hold the administration tube, with the other hand open the pinch clamp, and
+                                    graduallys
+                                    raise to allow the product to flow via gravity.</p>
+                            </div>
+                            <div class="fancyCard__flexContent">
+                                <h3 class="fancyCard__header">
+                                    AFTER
+                                </h3>
+                                <p>After instilling product, withdraw tube, and keep patient in position for up to
+                                    15 minutes to minimize possible cramping.</p>
+                                <p>Some product will remain in the tube; this is normal and does not affect
+                                    treatment.<sup>1</sup>
+                                </p>
+                            </div>
+                        </div>
+                        <hr class="tab__underline">
+                        <h3 class="fancyCard__header">
+                            important considerations<sup>1</sup>
+                        </h3>
+                        <ul class="fancyCard__list">
+                            <li><strong>DO NOT</strong> administer REBYOTA during antibiotic treatment for CDI </li>
+                            <li>It is recommended to administer REBYOTA from 24 to approximately 72 hours after the last
+                                dose of antibiotics</li>
+                            <li>The REBYOTA carton can be stored either in an ultracold freezer, -76°F to -130°F until
+                                its expiration or stored in the refrigerator, 36°F to 46°F for up to 4 days</li>
+                            <li><strong>Prior to use,</strong> thaw REBYOTA completely by placing the carton in either a
+                                refrigerator, 36°F to 46°F for approximately 24 hours or placing the carton at room
+                                temperature until thawed</li>
+
+                        </ul>
+                        <div class="fancyCard__button">
+                            <a href="#" class="button">FULL Instructions For Use
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+
+
+        </section>
+
+        <section class="cardBlock">
+            <div class="container">
+                <div class="cardBlock__wrapper">
+
+                    <div class="cardBlock__card">
+                        <figure class="cardBlock__img">
+                            <img src="<?php echo get_template_directory_uri() ?>/uploads/card.jpg" alt="Card Image" />
+                        </figure>
+                        <h2 class="cardblock__title">
+                            REBYOTA Clinical Data
+                        </h2>
+                        <p>Explore clinical data evaluating the efficacy
+                            and safety of REBYOTA to treat
+                            recurrent C. difficile infection (rCDI). </p>
+                        <a href="#" class="button">Clinical Data</a>
+                    </div>
+
+                    <div class="cardBlock__card">
+                        <figure class="cardBlock__img">
+                            <img src="<?php echo get_template_directory_uri() ?>/uploads/card.jpg" alt="Card Image" />
+                        </figure>
+                        <h2 class="cardblock__title">
+                            REBYOTA ADMINISTRATION
+                        </h2>
+                        <p>Learn how to administer REBYOTA—one dose, one complete microbiome treatment. </p>
+                        <a href="#" class="button">Administration</a>
+                    </div>
+
+                </div>
+            </div>
+        </section>
 
     </div>
+    <section class="accordion">
+            <div class="accordion__top">
+                <div class="container">
+                    <div class="accordion__topWrapper">
+                        <h2 class="accordion__topTitle">Important Safety information</h2>
+                        <div class="accordion__accoridoinButton" type="button">
+                            <svg class="icon accordion__icon icon-minus">
+                                <use xlink:href="#icon-minus"></use>
+                            </svg>
+                            <button class="accordion__plus" type="button">
+                                <svg class="icon accordion__icon icon-plus">
+                                    <use xlink:href="#icon-plus"></use>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion__body">
+                <div class="container">
+                    <div class="accordion__wrap">
+                        <h3 class="accordion__title">Indication For Use</h3>
+                        <p>REBYOTA (bacrodiome-mcrb) is indicated to reduce the recurrence of Clostridioides difficile
+                            infection (CDI) in adults following antibiotic treatment for recurrent CDI.</p>
+                        <h3 class="accordion__title">Important Safety Information</h3>
+                        <p><strong class="accordion__subTitle">Contraindications</strong></p>
+                        <p>REBYOTA has no identified contraindications.</p>
+                        <p><strong class="accordion__subTitle">Warnings and Precautions</strong></p>
+                        <p>No serious hypersensitivity reactions have been reported in patients treated with REBYOTA. Do
+                            not use in patients with a previous hypersensitivity to REBYOTA or any of the product
+                            components.</p>
+                        <p><strong class="accordion__subTitle">Adverse Reactions</strong></p>
+                        <p>The most common adverse reactions following blinded treatment with REBYOTA (reported by ≥5%
+                            of all subjects and greater than placebo) were diarrhea and abdominal pain.</p>
+                        <p><strong class="accordion__subTitle"> Interactions</strong></p>
+                        <p>Since REBYOTA is not absorbed into the body, no metabolic drug-drug interactions are
+                            expected.</p>
+                        <p><strong class="accordion__subTitle">Use In Specific Populations</strong></p>
+                        <p class="accordion__subTitle">Geriatric Use</p>
+                        <p>Of the 749 patients treated with REBYOTA, 47.9% were ≥65 years of age, and 25.8% were ≥75
+                            years of age. No overall differences in safety and efficacy were observed between these
+                            subjects and younger subjects. No dose adjustment is necessary for patients ≥65 years of
+                            age.</p>
+                        <p>You are encouraged to report negative side effects of prescription drugs to FDA. Visit
+                            www.FDA.gov/medwatch, or call 1-800-FDA-1088. </p>
+                        <a href="#" class="accordion__link">
+                            <p>Please click here for full Prescribing Information. </p>
+                        </a>
+                    </div>
+                </div>
+                <div class="accordion__bottom">
+                    <div class="container">
+                        <h3 class="accordion__bottomTitle">References</h3>
+                        <ol class="accordion__list">
+                            <li>Rebyota. Prescribing Information. Parsippany, NJ: Ferring Pharmaceuticals Inc; 2022.
+                            </li>
+                            <li>Sicard J-F, Le Bihan G, Vogeleer P, Jacques M, Harel J. Interactions of intestinal
+                                bacteria
+                                with components of intestinal mucus. Front Call Infect Microbiol. 2017;7:387.
+                                doi:10.3389/fcimb.2017.00387
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
 
 </main>
 <?php get_footer(); ?>
